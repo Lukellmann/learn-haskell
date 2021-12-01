d37b8e376c1d9ac03393a5b3de0b68e4694cf3dc
Lukellmann <lukellmann@gmail.com>
2021-12-01 23:49:51 +0100

    Chapter "Next steps"

---
+++ haskell_basics/next_steps.hs
@@ -0,0 +1,31 @@
+mySignum x
+  | x < 0 = -1
+  | x > 0 = 1
+  | otherwise = 0
+
+pts :: Int -> Int
+pts 1 = 10
+pts 2 = 6
+pts x
+  | x <= 6 = 7 - x
+  | otherwise = 0
+
+fst' :: (a, b) -> a
+fst' (x, _) = x
+
+snd' :: (a, b) -> b
+snd' (_, x) = x
+
+head' :: [a] -> a
+head' (x : _) = x
+head' [] = error "head: empty list"
+
+tail' :: [a] -> [a]
+tail' (_ : xs) = xs
+tail' [] = error "tail: empty list"
+
+roots :: Floating b => b -> b -> b -> (b, b)
+roots a b c =
+  let sdisc = sqrt (b * b - 4 * a * c)
+      twiceA = 2 * a
+   in ((-b + sdisc) / twiceA, (-b - sdisc) / twiceA)
